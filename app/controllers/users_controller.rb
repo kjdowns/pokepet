8a711436a9982b44821e326012d3d1be35eb0ce1
kjdowns <kevin.downs04@gmail.com>
2020-02-11 20:11:04 -0500

    stub out new and create actions for user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
     
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id 
+        end
+    end
+
+    private
+
+    def user_params
+        
+    end
+
 end
