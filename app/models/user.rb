0dded2b70195f1798d71df7bf93b14dbdf3b8f02
kjdowns <kevin.downs04@gmail.com>
2020-02-12 13:10:57 -0500

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :adoptions
     has_many :pokepets, through: :adoptions 
     has_secure_password
+    validates :user_name, presence: true
+    validates :user_name, uniqueness: true
 end
