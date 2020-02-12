130070cab4d1bc7c9d8cf99fe0c5dc235f4f4eb9
kjdowns <kevin.downs04@gmail.com>
2020-02-12 16:55:56 -0500

    edit sessions controller to handle user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(params[:user_name])
-        if @user.authenticate
+        @user = User.find_by(user_name: params[:user_name])
+        if @user.authenticate(params[:password])
             session[:user_id] = @user.id
         end
     end
