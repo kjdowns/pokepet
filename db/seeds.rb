926816c1707001d21cf744908d961eab8f1e6942
kjdowns <kevin.downs04@gmail.com>
2020-02-12 14:56:34 -0500

    add more items to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,11 @@
 PokePet.create(name: "Bulbasaur", element: "Grass", image: "https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/240px-001Bulbasaur.png", max_level: 15)
 PokePet.create(name: "Charmander", element: "Fire", image: "https://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/240px-004Charmander.png", max_level: 16)
 PokePet.create(name: "Squirtle", element: "Water", image: "https://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/240px-007Squirtle.png", max_level: 18)
+
+PokePet.create(name: "Chikorita", element: "Grass", image: "https://cdn.bulbagarden.net/upload/thumb/b/bf/152Chikorita.png/240px-152Chikorita.png", max_level: 15)
+PokePet.create(name: "Cyndaquil", element: "Fire", image: "https://cdn.bulbagarden.net/upload/thumb/9/9b/155Cyndaquil.png/240px-155Cyndaquil.png", max_level: 16)
+PokePet.create(name: "Totodile", element: "Water", image: "https://cdn.bulbagarden.net/upload/thumb/d/df/158Totodile.png/240px-158Totodile.png", max_level: 18)
+
+PokePet.create(name: "Treeko", element: "Grass", image: "https://cdn.bulbagarden.net/upload/thumb/2/2c/252Treecko.png/240px-252Treecko.png", max_level: 15)
+PokePet.create(name: "Torchic", element: "Fire", image: "https://cdn.bulbagarden.net/upload/thumb/9/91/255Torchic.png/240px-255Torchic.png", max_level: 16)
+PokePet.create(name: "Mudkip", element: "Water", image: "https://cdn.bulbagarden.net/upload/thumb/6/60/258Mudkip.png/240px-258Mudkip.png", max_level: 18)
