422358780ce4008f9ff2986ee6129bc8bf56e82d
kjdowns <kevin.downs04@gmail.com>
2020-02-11 20:05:17 -0500

    add basic routes for all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'sessions#home'
+  resources :users
+  resources :poke_pets
+  resources :adoptions
+  get '/signin', to: 'sessions#signin'
+  post '/signin', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 end
