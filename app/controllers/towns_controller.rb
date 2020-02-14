a30c3844fece68cb06e0cbdbe957bc3db751ebbc
kjdowns <kevin.downs04@gmail.com>
2020-02-13 20:16:55 -0500

    add towns controller, routes, and index page

---
+++ app/controllers/towns_controller.rb
@@ -0,0 +1,7 @@
+class TownsController < ApplicationController
+
+    def index
+        
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   root 'sessions#home'
+
   resources :users
   resources :poke_pets
   resources :adoptions
+  resources :towns
+
   get '/signin', to: 'sessions#signin'
   post '/signin', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
   get '/logout', to: 'sessions#destroy'
+
   get '/adoptions/poke_pet/:poke_pet_id', to: 'adoptions#new_pet', as: 'pet_adoption'
 end
