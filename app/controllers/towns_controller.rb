ae2dee4aa07596211e53af30987961ff36a197eb
kjdowns <kevin.downs04@gmail.com>
2020-02-17 02:48:56 -0500

    add stubs for towns post actions

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -1,15 +1,15 @@
 class TownsController < ApplicationController
 
     def bank_action
-        
+        redirect_to bank_path, notice: "You withdrew 1000 PokeDollars from the bank!"
     end
 
     def shop_action
-        
+        redirect_to shop_path
     end
 
-    def park_action
-        
+    def training_action
+        redirect_to training_path
     end
     
 end
