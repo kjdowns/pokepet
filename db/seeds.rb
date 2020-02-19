b12ecd5ad079c1665bd3a0bc1fefc6acda3e3e5a
kjdowns <kevin.downs04@gmail.com>
2020-02-19 17:03:06 -0500

    destroy all PokePets and re-seed table with production values

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,26 @@ PokePet.create(name: "Totodile", element: "Water", image: "https://cdn.bulbagard
 PokePet.create(name: "Treeko", element: "Grass", image: "https://cdn.bulbagarden.net/upload/thumb/2/2c/252Treecko.png/240px-252Treecko.png", max_level: 15)
 PokePet.create(name: "Torchic", element: "Fire", image: "https://cdn.bulbagarden.net/upload/thumb/9/91/255Torchic.png/240px-255Torchic.png", max_level: 16)
 PokePet.create(name: "Mudkip", element: "Water", image: "https://cdn.bulbagarden.net/upload/thumb/6/60/258Mudkip.png/240px-258Mudkip.png", max_level: 18)
+
+PokePet.create(name: "Turtwig", element: "Grass", image: "https://cdn.bulbagarden.net/upload/thumb/5/5c/387Turtwig.png/240px-387Turtwig.png", max_level: 15)
+PokePet.create(name: "Chimchar", element: "Fire", image: "https://cdn.bulbagarden.net/upload/thumb/7/76/390Chimchar.png/240px-390Chimchar.png", max_level: 16)
+PokePet.create(name: "Piplup", element: "Water", image: "https://cdn.bulbagarden.net/upload/thumb/b/b1/393Piplup.png/240px-393Piplup.png", max_level: 18)
+
+PokePet.create(name: "Snivy", element: "Grass", image: "https://cdn.bulbagarden.net/upload/thumb/7/75/495Snivy.png/240px-495Snivy.png", max_level: 15)
+PokePet.create(name: "Tepig", element: "Fire", image: "https://cdn.bulbagarden.net/upload/thumb/5/5b/498Tepig.png/240px-498Tepig.png", max_level: 16)
+PokePet.create(name: "Oshawott", element: "Water", image: "https://cdn.bulbagarden.net/upload/thumb/3/3b/501Oshawott.png/240px-501Oshawott.png", max_level: 18)
+
+PokePet.create(name: "Chespin", element: "Grass", image: "https://cdn.bulbagarden.net/upload/thumb/c/ca/650Chespin.png/240px-650Chespin.png", max_level: 15)
+PokePet.create(name: "Fennekin", element: "Fire", image: "https://cdn.bulbagarden.net/upload/thumb/3/3d/653Fennekin.png/240px-653Fennekin.png", max_level: 16)
+PokePet.create(name: "Froakie", element: "Water", image: "https://cdn.bulbagarden.net/upload/thumb/1/18/656Froakie.png/240px-656Froakie.png", max_level: 18)
+
+PokePet.create(name: "Rowlett", element: "Grass", image: "https://cdn.bulbagarden.net/upload/thumb/7/74/722Rowlet.png/240px-722Rowlet.png", max_level: 15)
+PokePet.create(name: "Litten", element: "Fire", image: "https://cdn.bulbagarden.net/upload/thumb/0/0e/725Litten.png/240px-725Litten.png", max_level: 16)
+PokePet.create(name: "Popplio", element: "Water", image: "https://cdn.bulbagarden.net/upload/thumb/d/d8/728Popplio.png/240px-728Popplio.png", max_level: 18)
+
+PokePet.create(name: "Grookey", element: "Grass", image: "https://cdn.bulbagarden.net/upload/thumb/9/93/810Grookey.png/240px-810Grookey.png", max_level: 15)
+PokePet.create(name: "Scorbunny", element: "Fire", image: "https://cdn.bulbagarden.net/upload/thumb/0/06/813Scorbunny.png/240px-813Scorbunny.png", max_level: 16)
+PokePet.create(name: "Sobble", element: "Water", image: "https://cdn.bulbagarden.net/upload/thumb/9/9b/816Sobble.png/240px-816Sobble.png", max_level: 18)
+
+PokePet.create(name: "Pikachu", element: "Electric", image: "https://cdn.bulbagarden.net/upload/thumb/b/b8/025Pikachu_LG.png/240px-025Pikachu_LG.png", max_level: 20)
+PokePet.create(name: "Eevee", element: "Normal", image: "https://cdn.bulbagarden.net/upload/thumb/6/6e/133Eevee_LG.png/240px-133Eevee_LG.png", max_level: 20)
